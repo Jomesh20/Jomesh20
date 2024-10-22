71401cf91a48deb3df881f6de0a8e2fe4a168aa6
Jomesh20 <95348492+Jomesh20@users.noreply.github.com>
2024-10-22 18:24:41 +0100

    Add files via upload
    This suggests a band name by merging both the city you grew up in and suggested pet name by concatenating both input

---
+++ Day1_Project_Band_Generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+city = (input("What city did you grow up in?\n"))
+pet = (input("Suggest any pet name that comes to mind\n"))
+print("Your suggested band name is: " + city + " " + pet)
