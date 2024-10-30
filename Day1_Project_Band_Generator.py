628b09cb965a552cde1d5cedefd45429310dc557
Jomesh20 <95348492+Jomesh20@users.noreply.github.com>
2024-10-30 22:28:20 +0100

    Delete Day1_Project_Band_Generator.py

--- Day1_Project_Band_Generator.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to the Band Name Generator")
-city = (input("What city did you grow up in?\n"))
-pet = (input("Suggest any pet name that comes to mind\n"))
-print("Your suggested band name is: " + city + " " + pet)
